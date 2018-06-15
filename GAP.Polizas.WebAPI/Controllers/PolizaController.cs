1aa05385bed42efd737b355f71a7f96fdf6e8814
Walter Estrada <waly13@hotmail.com>
2018-06-15 10:40:56 -0500

    Se crea una BD de Polizas con las tablas Clientes, Pólizas, TipoCubrimiento, TipoRiesgo. Se instala Ninject para la inyección de dependencias y se inicia la implementación de la Controladora de Pólizas

---
+++ GAP.Polizas.WebAPI/Controllers/PolizaController.cs
@@ -0,0 +1,34 @@
+﻿using GAP.Polizas.ContratoLogicaNegocio;
+using GAP.Polizas.LogicaNegocio;
+using GAP.Polizas.Modelo;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace GAP.Polizas.WebAPI.Controllers
+{
+    [RoutePrefix("api/poliza")]
+    public class PolizaController : ApiController
+    {
+        IPoliza ipoliza;
+        public PolizaController(IPoliza poliza)
+        {
+            this.ipoliza = poliza;
+        }
+
+        public async Task<HttpResponseMessage> GuardarPoliza(Poliza poliza)
+        {
+            return Request.CreateResponse(HttpStatusCode.BadRequest, "");
+        }
+        public async Task<HttpResponseMessage> ConsultarPolizas()
+        {
+            return Request.CreateResponse(HttpStatusCode.BadRequest, "");
+        }
+        public async Task<HttpResponseMessage> EliminarPoliza(int id)
+        {
+            return Request.CreateResponse(HttpStatusCode.BadRequest, "");
+        }
+       
+    }
+}
